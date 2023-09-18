5f243e3ac1e6f57e44eb4ca0c8b00b16ba440261
Rian Finnegan <rian@blinq.me>
2023-09-19 08:51:03 +1000

    actually write to public

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
 chmod +x dotnet-install.sh
 ./dotnet-install.sh -c 8.0 -InstallDir ./dotnet
 ./dotnet/dotnet --version
-./dotnet/dotnet publish app/edge -c Release -o output
+./dotnet/dotnet publish app/edge -c Release -o public
