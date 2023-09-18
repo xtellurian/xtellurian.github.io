47755aa48a5872a53b8873efab5e491f578498c0
Rian Finnegan <rian@blinq.me>
2023-09-19 08:49:18 +1000

    get started edge

--- build.sh
+++ build.sh
@@ -1,1 +1,6 @@
-yarn build
+#!/bin/sh
+curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
+chmod +x dotnet-install.sh
+./dotnet-install.sh -c 8.0 -InstallDir ./dotnet
+./dotnet/dotnet --version
+./dotnet/dotnet publish app/edge -c Release -o output
