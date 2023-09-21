5079935fa16b5bdbdfb5b30924fbb7d3526adb63
Rian Finnegan <rian@blinq.me>
2023-09-21 14:54:38 +1000

    fix: path

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
 chmod +x dotnet-install.sh
 ./dotnet-install.sh -c 7.0 -InstallDir ./dotnet
 ./dotnet/dotnet --version
-./dotnet/dotnet publish app/edge -c Release -o public
+./dotnet/dotnet publish edge -c Release -o public
