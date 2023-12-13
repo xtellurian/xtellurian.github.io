6e5c8357dfed70f37733133308bf7049610b1c8e
Rian <rian@finnegan.cc>
2023-12-13 22:01:46 +1100

    add serverside blazor and azure webapp

--- edge/Pages/Post.razor.js
+++
@@ -1,7 +0,0 @@
-﻿export async function setInnerHtmlFromRequest(id, url) {
-  const converter = new showdown.Converter(); 
-  const content = await fetch(url);
-  const text = await content.text();
-  const html = converter.makeHtml(text);
-  document.getElementById(id).innerHTML = html;
-}
